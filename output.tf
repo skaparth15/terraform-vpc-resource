8b8722383bbce35c1d77f127e7d22aee373d98fa
sri charan <sricharankaparthi123@gmail.com>
2024-06-22 16:14:07 +0530

    new files

--- output.tf
+++ output.tf
@@ -5,3 +5,17 @@ output "zone_availables" {
 output "vpc_id" {
   value = aws_vpc.vpc-expense.id
 }
+
+
+output "backend_subnet_id" {
+  value = aws_subnet.backend-expense.id
+}
+
+output "database_subnet_id" {
+  value = aws_subnet.database-expense.id
+}
+
+output "frontend-subnet-id" {
+  value = aws_subnet.frontend-expense.id
+}
+
