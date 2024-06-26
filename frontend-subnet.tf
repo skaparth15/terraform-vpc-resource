5135b35568633932b359e2ff330b050bd8650e45
sri charan <sricharankaparthi123@gmail.com>
2024-06-26 22:26:55 +0530

    some-message

--- frontend-subnet.tf
+++ frontend-subnet.tf
@@ -1,6 +1,7 @@
 resource "aws_subnet" "frontend-expense" {
   count = length(var.frontend_subnet_cidrs)
   vpc_id     = aws_vpc.vpc-expense.id
+  map_public_ip_on_launch = true
   cidr_block = var.frontend_subnet_cidrs[count.index]
   availability_zone = data.aws_availability_zones.available.names[count.index]
 
